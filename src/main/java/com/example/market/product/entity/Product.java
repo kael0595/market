d91e06a12bee292aeae0841b1db699d742029b11
Kael0595 <ants7021@naver.com>
2023-08-07 11:27:57 +0900

    member, product entity 작성

--- src/main/java/com/example/market/product/entity/Product.java
+++ src/main/java/com/example/market/product/entity/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
 
@@ -18,7 +20,9 @@ public class Product {
 
     private int price;
 
+    @CreatedDate
     private LocalDate createDate;
 
+    @LastModifiedDate
     private LocalDate modifyDate;
 }
