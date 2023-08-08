be6b333729b224a16905386f8886cacf751bf3e1
Kael0595 <ants7021@naver.com>
2023-08-08 09:25:52 +0900

    join, login UX/UI 세팅

--- src/main/java/com/example/market/member/controller/MemberController.java
+++ src/main/java/com/example/market/member/controller/MemberController.java
@@ -3,21 +3,18 @@ package com.example.market.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
     @GetMapping("/login")
-    @ResponseBody
     public String login() {
-        return "로그인 페이지입니다.";
+        return "/member/login";
     }
 
     @GetMapping("/join")
-    @ResponseBody
     public String join() {
-        return "회원가입 페이지입니다.";
+        return "/member/join";
     }
 }
